91603e468e67f2eaea3b401422a6b714c9df4bbd
Mitchell Long <meechllada@gmail.com>
2017-09-22 10:00:13 -0700

    return project title and returns the id of a project before saving it

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,25 +1,25 @@
 require "spec_helper"
 
 # describe Project do
-#   describe '#title' do
-#     it 'returns the project title' do
-#       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-#       expect(project.title).to eq 'Teaching Kids to Code'
-#     end
-#   end
-#
-#   context '#id' do
-#     it 'returns the id of the project before saving project' do
-#       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-#       expect(project.id).to eq nil
-#     end
-#
-#     it 'returns the id of the project after saving project' do
-#       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-#       project.save
-#       expect(project.id).to be_an_instance_of Integer
-#     end
-#   end
+  describe '#title' do
+    it 'returns the project title' do
+      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      expect(project.title).to eq 'Teaching Kids to Code'
+    end
+  end
+
+  context '#id' do
+    it 'returns the id of the project before saving project' do
+      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      expect(project.id).to eq nil
+    end
+
+    it 'returns the id of the project after saving project' do
+      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      project.save
+      expect(project.id).to be_an_instance_of Integer
+    end
+  end
 
   describe '#==' do
     it 'is the same project if two projects have the same title' do
