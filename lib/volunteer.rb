0f55c64a141f7d1ade3182f653a2e3c6ff1e81c7
Mitchell Long <meechllada@gmail.com>
2017-09-22 13:00:11 -0700

    adds passing #save and .all methods

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,10 +1,22 @@
 class Volunteer
-  attr_accessor :name, :project_id, :id
+attr_accessor(:name, :project_id, :id)
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @project_id = attributes.fetch(:project_id)
-    @id = attributes.fetch(:id)
+    @id = nil
+  end
+  def self.all
+    returned_volunteers = DB.exec('SELECT * FROM volunteers;')
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({name: name, project_id: project_id, id: id}))
+
+    end
+    volunteers
   end
 
   def save
@@ -14,8 +26,6 @@ class Volunteer
   def ==(another_volunteer)
     self.name().==(another_volunteer.name()).&(self.id().==(another_volunteer.id()))
   end
-
-
 
 
 end
